69f15de2fe65c56df045a6539614cff4c63acaf3
James King <metapyziks@gmail.com>
2013-12-03 21:17:27 +0000

    Initial commit

---
+++ ProceduralMusic/Source.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProceduralMusic
+{
+    public abstract class Source
+    {
+        public static long SampleCount(double duration, double freq)
+        {
+            return (long) Math.Ceiling(duration / freq);
+        }
+
+        public abstract float Sample(double t);
+        public virtual void Sample(float[] buffer, double start, double duration, double freq)
+        {
+            Sample(buffer, 0, start, duration, freq);
+        }
+
+        public virtual void Sample(float[] buffer, int offset, double start, double duration, double freq)
+        {
+            long samples = Math.Min(buffer.Length - offset, SampleCount(duration, freq));
+            for (long i = samples - 1; i >= 0; --i) {
+                buffer[i + offset] = Sample(start + i * freq);
+            }
+        }
+    }
+}
