03ba39c9021b9fa44abc29ba9b56b26c9102e91c
James King <metapyziks@gmail.com>
2013-12-07 01:58:04 +0000

    Improved choon

--- DrumLoopA/Program.cs
+++ DrumLoopA/Program.cs
@@ -8,7 +8,7 @@ namespace DrumLoopA
     {
         static void Main(string[] args)
         {
-            var bpm = 170.0;
+            var bpm = 140.0;
 
             var sampleDir = Path.Combine("..", "..", "..", "Samples");
             var drumDir = Path.Combine(sampleDir, "DrumHits");
@@ -16,21 +16,26 @@ namespace DrumLoopA
             var snareDir = Path.Combine(drumDir, "Snares");
             var bassDrumDir = Path.Combine(drumDir, "BassDrums");
             var synthDir = Path.Combine(sampleDir, "Synth");
+            var sfxDir = Path.Combine(sampleDir, "SFX");
             var bassDir = Path.Combine(sampleDir, "BassSounds");
 
             var bass = new SampleFile(Path.Combine(bassDir, "DJF_C_SEXUAL808_BASS.wav")).Scale(0.2);
-            var pad = new SampleFile(Path.Combine(synthDir, "DJF_C_SUNRISE.wav")).Scale(0.1);
+            var pad = new SampleFile(Path.Combine(synthDir, "DJF_C_SUNRISE.wav")).Scale(0.1)
+                .PitchShift(0.25);
+            var sfxA = new SampleFile(Path.Combine(sfxDir, "DJF_ALIEN_RECON.wav")).Scale(0.1)
+                .PitchShift(0.25);
+            var sfxB = new SampleFile(Path.Combine(sfxDir, "DJF_AIRLOCK.wav")).Scale(0.2)
+                .PitchShift(0.25);
             
             var hiHat = new SampleFile(Path.Combine(hiHatDir, "DJF_HH_HINGE.wav")).Scale(0.4);
             var crash = new SampleFile(Path.Combine(hiHatDir, "DJF_HH_JINAL.wav")).Scale(0.1);
             var snare = new SampleFile(Path.Combine(hiHatDir, "DJF_HH_HEAT_BEAST.wav")).Scale(0.27);
             var bassDrum = new SampleFile(Path.Combine(bassDrumDir, "DJF_BD_KNOCKERS.wav")).Scale(0.33);
 
-            Func<Source, Source> fade = (x) => x
-                .FadeIn(15.0 / bpm).PhaseShift(-60.0 / bpm * 12)
+            Func<Source, Source> fade = (x) => x.PhaseShift(-60.0 / bpm * 12)
                 .FadeOut(60.0 / bpm * 12).PhaseShift(60.0 / bpm * 12);
 
-            var seq = new Sequence(bpm / 64.0) {
+            var seq = new Sequence(bpm / 16.0) {
                 { 0, new Sequence(bpm / 16.0) {
                     { 0, bass },
                     { 0, new Sequence(bpm) {
@@ -65,19 +70,21 @@ namespace DrumLoopA
                         { 4,  snare },
                         { 7,  snare },
                         { 9,  snare },
-                        { 10, crash.Echo(30.0 / bpm, 0.7) },
+                        { 10, crash.Echo(30.0 / bpm, 0.7).FadeIn(30.0 / bpm) },
                         { 10, bassDrum },
                         { 12, hiHat.Repeat(bpm, 2, 2) },
                         { 14, snare }
                     } },
                 }.Repeat(bpm, 64, 4) },
                 { 0, fade(pad) },
-                { 1, fade(pad.PitchShift(0.84089622337261497225281353842922)) },
-                { 2, fade(pad) },
-                { 3, fade(pad.PitchShift(1.0594623519771811343021158573439)) },
+                { 1, sfxA },
+                { 4, fade(pad.PitchShift(0.84089622337261497225281353842922)) },
+                { 8, fade(pad) },
+                { 11, sfxB.PhaseShift(-240 / bpm).FadeOut(-240 / bpm).PhaseShift(120 / bpm) },
+                { 12, fade(pad.PitchShift(1.0594623519771811343021158573439)) },
             };
 
-            Sampler.PreviewPlayback(seq, 0.0, 15.0 / bpm * 272);
+            Sampler.PreviewPlayback(seq, 0.0, 15.0 / bpm * 288);
         }
     }
 }
